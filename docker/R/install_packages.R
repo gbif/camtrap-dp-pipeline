8cdc99e675c1dfae4e5e83615acb8623a05f4cfa
fmendezh <federicomh@gmail.com>
2022-10-10 14:01:40 +0200

    using devtools to install R camtraptor

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,15 +1,28 @@
-options(
-  HTTPUserAgent =
-    sprintf(
-      "R/%s R (%s)",
-      getRversion(),
-      paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])
-    )
-)
+install.packages(c("assertthat",
+                   "DBI",
+                   "dplyr",
+                   "EML",
+                   "frictionless",
+                   "glue",
+                   "htmltools",
+                   "leaflet",
+                   "lifecycle",
+                   "lubridate",
+                   "purrr",
+                   "RColorBrewer",
+                   "readr",
+                   "rlang",
+                   "RSQLite",
+                   "stringr",
+                   "tidyr",
+                   "tools",
+                   "utils",
+                   "uuid",
+                   "covr",
+                   "knitr",
+                   "rmarkdown",
+                   "testthat",
+                   "knitr",
+                   "devtools"), dependencies = TRUE)
 
-install.packages("arrow", repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
-
-install.packages(c("dplyr","here","EML","plumber", "assertthat", "datapackage", "dplyr", "glue", "here",
-                   "htmltools", "jsonlite", "leaflet", "lubridate", "purrr", "readr", "rlang", "stringr", "tidyr",
-                   "datapackage", "leaflet", "config", "iterators", "jsonvalidate", "rlist", "tableschema.r",
-                   "urltools", "V8"), dependencies = TRUE)
+devtools::install_github("inbo/camtraptor")

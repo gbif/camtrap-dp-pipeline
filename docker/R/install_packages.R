74134c2920bce3e7773366d29036c4b97b0083d0
fmendezh <federicomh@gmail.com>
2022-10-11 15:35:29 +0200

    moving R packages as system dependencies

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,3 +1,2 @@
-install.packages(c("jqr","leaflet", "EML", "emld","frictionless", "readr", "emld", "viridis", "vroom","rmarkdown", "jsonld","ggplot2","tidyselect","leaflet.providers","purrr","evaluate","curl","isoband","lifecycle"),
-                 dependencies = TRUE, libs_only= TRUE)
+install.packages(c("jqr","leaflet","leaflet.providers","EML","emld","frictionless","R.methodsS3"), dependencies = TRUE, libs_only= TRUE)
 remotes::install_github("inbo/camtraptor", build_vignettes = FALSE, build_manual = FALSE)

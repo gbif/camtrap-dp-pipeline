df1dbbd4693df33bcd901edcbc12f0f73251a43f
fmendezh <federicomh@gmail.com>
2022-10-11 23:14:32 +0200

    adding dependencies as system packages

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,2 +1,13 @@
+remotes::install_github("cboettig/arkdb")
+options(
+  HTTPUserAgent =
+    sprintf(
+      "R/%s R (%s)",
+      getRversion(),
+      paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])
+    )
+)
+Sys.setenv(LIBARROW_BINARY = "true", LIBARROW_MINIMAL = "false")
+install.packages("arrow", repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
 install.packages(c("jqr","leaflet","leaflet.providers","EML","emld","frictionless","R.methodsS3","duckdb","contentid","RJSONIO","rgeos","storr","nycflights13","RPostgres"), dependencies = TRUE)
 remotes::install_github("inbo/camtraptor", build_vignettes = FALSE, build_manual = FALSE)

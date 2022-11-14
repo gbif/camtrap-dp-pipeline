d93d7267d5b83830d4d68e7b8a0b05a09e1c4ec9
Matthew Blissett <mblissett@gbif.org>
2022-11-14 15:17:27 +0100

    Use supported R 4.2.2 base image, and specific release of arkdb.
    Check camtraptor has installed and exit if not.

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,9 +1,9 @@
-#https://github.com/ropensci/arkdb
-#Arkdb installation from source
-remotes::install_github("cboettig/arkdb")
+# Arkdb installation from source
+# https://github.com/ropensci/arkdb
+remotes::install_github("cboettig/arkdb@v0.0.14")
 
-#https://arrow.apache.org/docs/r/articles/install.html
-#Installing Apache Arrow from binaries
+# Installing Apache Arrow from binaries
+# https://arrow.apache.org/docs/r/articles/install.html
 options(
   HTTPUserAgent =
     sprintf(
@@ -15,11 +15,15 @@ options(
 Sys.setenv(LIBARROW_BINARY = "true", LIBARROW_MINIMAL = "false")
 install.packages("arrow", repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest", libs_only = TRUE)
 
-#Install source packages
-install.packages(c("jqr","leaflet","leaflet.providers","EML","emld","R.methodsS3","contentid","RJSONIO","rgeos","storr"), dependencies = FALSE, libs_only = TRUE)
 
 # Or rOpenSci R-universe
 install.packages("frictionless", repos = "https://ropensci.r-universe.dev")
+if ( ! library("frictionless", character.only=TRUE, logical.return=TRUE) ) {
+  quit(status=1, save='no')
+}
 
-#Install camtraptor package from github
+# Install camtraptor package from GitHub
 remotes::install_github("inbo/camtraptor", build_vignettes = FALSE, build_manual = FALSE)
+if ( ! library("camtraptor", character.only=TRUE, logical.return=TRUE) ) {
+  quit(status=1, save='no')
+}

f8a6172b71030304189502f2bfc89bd8f47c7675
Peter Desmet <peter.desmet@inbo.be>
2024-09-30 09:44:59 +0200

    Install camtrapdp from R-universe

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,9 +1,8 @@
-# Install camtrapdp from CRAN
-#install.packages("camtrapdp")
-
-# Install camtraptdp  from GitHub
-install.packages("devtools")
-devtools::install_github("inbo/camtrapdp", ref = "refs/tags/v0.3.0")
+# Install camtrapdp from r-universe
+install.packages(
+  "camtrapdp",
+  repos = c("https://inbo.r-universe.dev", "https://cloud.r-project.org")
+)
 if (!library("camtrapdp", character.only = TRUE, logical.return = TRUE)) {
   quit(status = 1, save = "no")
 }

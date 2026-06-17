4aebe4a323d4860b5ffd0618cd3ff4ed6cc5b107
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2026-06-17 18:34:16 +0200

    chore: make sure the right version is taken from r-universe #10

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,8 +1,19 @@
+required_version <- "0.5.0"
+
 # Install camtrapdp from r-universe
 install.packages(
   "camtrapdp",
   repos = c("https://inbo.r-universe.dev", "https://cloud.r-project.org")
 )
+
+installed_version <- as.character(packageVersion("camtrapdp"))
+if (installed_version != required_version) {
+  stop(sprintf(
+    "Expected camtrapdp %s but r-universe/CRAN installed %s. Aborting build.",
+    required_version, installed_version
+  ))
+}
+
 if (!library("camtrapdp", character.only = TRUE, logical.return = TRUE)) {
   quit(status = 1, save = "no")
 }
